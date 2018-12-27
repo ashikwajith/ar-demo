ef857c3fa97e13b7416f613c48ee9a671a27af48
Ashikahmed Sheik <ashikahmed.sheik@aspiresys.com>
2018-12-27 14:21:13 +0530

    omitted local aframe

--- ar-service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => {
-    console.log('service worker installed');
-});
-
-self.addEventListener('activate', e => {
-    console.log('service worker activated');
-});
--- main.js
+++
@@ -1,8 +0,0 @@
-if('serviceWorker' in navigator) {
-    window.addEventListener('load', ()=> {
-        navigator.serviceWorker
-        .register('./ar-service-worker.js')
-        .then(reg => console.log("sw -> registered"))
-        .catch(err => console.log('sw -> not registered'));
-    });
-}
